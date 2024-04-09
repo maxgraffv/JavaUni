76f802fb1e80e1ab3e1823bafc86c4f921172e6e
Max <maksymilian.graff@gmail.com>
2024-04-09 22:34:40 +0200

    Task 5 added - HashMap

--- GUI3/Task4/Main.java
+++ GUI3/Task4/Main.java
@@ -4,7 +4,7 @@ class Main
 {
     public static void main(String[] args)
     {
-        
+        //TASK 4
         HashSet<Integer> hSet = new HashSet<>();
         
         hSet.add(31);
@@ -22,6 +22,30 @@ class Main
 
         for(int e : hSet)
             System.out.println(e);
+
+
+        //TASK 5
+        HashMap<Integer, String> hMap = new HashMap<>();
+       
+        hMap.put(31, "Jan");
+        hMap.put(28, "Feb");
+        hMap.put(31, "Mar");
+        hMap.put(30, "Apr");
+        hMap.put(31, "May");
+        hMap.put(30, "Jun");
+        hMap.put(31, "Jul");
+        hMap.put(31, "Aug");
+        hMap.put(30, "Sep");
+        hMap.put(31, "Oct");
+        hMap.put(30, "Nov");
+        hMap.put(31, "Dec");
+
+
+        for(int i : hMap.keySet())
+            System.out.println("Key: " + i + " Val:" + hMap.get(i));
+
+
+        
        
     }
 }
