176c50e0512503e2f3c706f01567b3395928a53d
Max <maksymilian.graff@gmail.com>
2024-04-09 21:59:50 +0200

    Create Main.java

---
+++ GUI3/Task1/Main.java
@@ -0,0 +1,19 @@
+
+
+class Main
+{
+    public static void main(String[] args)
+    {
+
+        Container<Integer> myInt = new Container<>(5);
+        Container<String> myStr = new Container<>("String");
+        myInt.print();
+        myStr.print();
+
+    }
+
+
+
+
+
+}
